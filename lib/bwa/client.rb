328f77a739139ecc05e998a1f020bb79ce4b5401
Cody Cutrer <cody@cutrer.us>
2021-11-10 14:59:26 -0700

    adjust temperature limits based on testing
    currenttemperature can feasibly be outside the current set temperature range,
    so always advertise that it might be more.
    
    then adjust the set point range for fahrenheit, both high and low
    
    closes #41

--- lib/bwa/client.rb
+++ lib/bwa/client.rb
@@ -143,8 +143,8 @@ module BWA
       end
     end
 
-    # high range is 80-104 for F, 26-40 for C (by 0.5)
-    # low range is 50-80 for F, 10-26 for C (by 0.5)
+    # high range is 80-106 for F, 26-40 for C (by 0.5)
+    # low range is 50-99 for F, 10-26 for C (by 0.5)
     def set_temperature(desired)
       desired *= 2 if last_status && last_status.temperature_scale == :celsius || desired < 50
       send_message("\x0a\xbf\x20#{desired.round.chr}")
