54d2492b77e15b8c85a999132b29b1ee3c463111
Cody Cutrer <cody@cutrer.us>
2022-12-19 15:28:33 -0700

    fix set temperature scale
    fixes #69

--- lib/bwa/messages/set_temperature_scale.rb
+++ lib/bwa/messages/set_temperature_scale.rb
@@ -18,7 +18,7 @@ module BWA
       end
 
       def serialize
-        data = "\x01\x00"
+        data = +"\x01\x00"
         data[1] = (scale == :fahrenheit ? 0x00 : 0x01).chr
         super(data)
       end
